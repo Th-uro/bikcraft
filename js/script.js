f2cb7aef5bfb058eab866d465c0bcbc8106a8a51
Th-uro <arthurolivsira@gmail.com>
2024-07-08 20:01:52 -0300

    Adicionando parametros na URL para selecionar dinamicamente o serviço contratado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,15 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar intens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
