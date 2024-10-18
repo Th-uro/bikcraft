111e6ab194484e13fcb4b142f9e30afb499df3cf
Th-uro <arthurolivsira@gmail.com>
2024-10-17 22:51:02 -0300

    Adicionando formulário PHP e formulario.js

---
+++ js/formulario.js
@@ -0,0 +1,27 @@
+const formulario = document.querySelector("form");
+
+function formularioEnviado(resposta) {
+  if (resposta.ok) {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317A00;'>Menagem enviada</span>, em breve entraremosm em contato. Prazo de resposta em até 24 horas.</p>";
+  } else {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7:'><span style='color: #E00000;'>Erro no envio</span>, você pode enviar diretamente no nosso e-mail em: contato@bikcraft.net</p>";
+  }
+}
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector("form button");
+  botao.disabled = true;
+  botao.innerText = "Enviando...";
+
+  const data = new FormData(formulario);
+
+  fetch("./enviar.php", {
+    method: "POST",
+    body: data,
+  }).then(formularioEnviado);
+}
+
+formulario.addEventListener("submit", enviarFormulario);
